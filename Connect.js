a6ac9f362e3fee63e567a7971877f29ed2939a95
Subhashganni <113432507+Subhashganni@users.noreply.github.com>
2022-11-11 17:21:02 +0530

    Update Connect.js

--- Connect.js
+++ Connect.js
@@ -1,28 +1,22 @@
-import abi from"./abi/abi.json" assert { type: "json" };
+import abi from "./abi/abi.json" assert {type:"json"};
 
-const connect = new Promise((res, rej) => {
-    if(typeof window.eterium == "undefined"){
-        rej("Install Metamask");
-    }
-    window.eterium.request({method: "eth_requestAccounts" });
 
-    let web3 = new Web3(windoe.etherium);
-    let contract = new web3.eth.Contract(abi, "0xf815b437C47a643772014b2a5d97d3F81F51ef5F");
-
-    web3.eth.getAccounts().then((accounts) => {
-        contract.method
-        .totalSupply()
-        .call({ from: accounts[0]})
-        .then((supply) => {
-            contract.method
-            .getBuildings()
-            .call({ from: accounts[0]})
-            .then((data) => {
-                res({supply: supply, buildings: data });
-
-            });
-        });
-     });
+const connect = new Promise ((res,rej) => {
+if (typeof window.ethereum == "undefined"){
+    rej("install metamask");
+    
+}
+window.ethereum.request({method : "eth_requestAccounts"});
+let web3 = new Web3(window.ethereum);
+let contract = new web3.eth.Contract(abi,"0xf815b437C47a643772014b2a5d97d3F81F51ef5F");
+web3.eth.getAccounts().then((accounts) => {
+    contract.methods.totalSupply().call({from: accounts[0]}).then((supply)=>{
+        console.log(supply);
+        contract.methods.getBuildings().call({from: accounts[0]}).then((data)=>{
+            res({supply: supply,buildings : data});
+    })
 });
 
+});
+});
 export default connect;
